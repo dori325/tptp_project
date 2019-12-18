89f3515bf7a9acd0f0b680b5c62c5669ecfdf857
2Class11 <2Class11@2Class11>
2019-12-18 17:29:01 +0900

    news서비스 6개파일

---
+++ src/main/java/com/tptp/service/H_Service.java
@@ -0,0 +1,10 @@
+package com.tptp.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface H_Service {
+
+	List<Map<String, Object>> list(Map<String, Object> map) throws Exception;
+
+}
---
+++ src/main/java/com/tptp/service/H_ServiceImpl.java
@@ -0,0 +1,27 @@
+package com.tptp.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.tptp.dao.NewsDAO;
+
+@Service("tptpService")
+public class H_ServiceImpl implements H_Service {
+	Logger log = Logger.getLogger(this.getClass());
+
+	@Resource(name = "newsDAO")
+	private NewsDAO newsDAO;
+
+	@Override
+	public List<Map<String, Object>> list(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		return newsDAO.list(map);
+	}	
+	
+}
+//컨트롤러 - 서비스 - 서비스impl - dao ->sql.xml
