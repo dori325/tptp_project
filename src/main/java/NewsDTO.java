5389ba0912bd5ca8c03d5b7251702d8b73440027
2Class11 <2Class11@2Class11>
2019-12-18 17:52:59 +0900

    news service 7개

---
+++ src/main/java/NewsDTO.java
@@ -0,0 +1,75 @@
+
+
+public class NewsDTO {
+
+	private int b_no;
+	private String b_title;
+	private String b_content;
+	private int b_count;
+	private String b_date;
+	private int b_like;
+	private String b_modidate;
+	private String b_cate1;
+	private String b_cate2;
+	private String b_file;
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public String getB_title() {
+		return b_title;
+	}
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
+	}
+	public String getB_content() {
+		return b_content;
+	}
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
+	}
+	public int getB_count() {
+		return b_count;
+	}
+	public void setB_count(int b_count) {
+		this.b_count = b_count;
+	}
+	public String getB_date() {
+		return b_date;
+	}
+	public void setB_date(String b_date) {
+		this.b_date = b_date;
+	}
+	public int getB_like() {
+		return b_like;
+	}
+	public void setB_like(int b_like) {
+		this.b_like = b_like;
+	}
+	public String getB_modidate() {
+		return b_modidate;
+	}
+	public void setB_modidate(String b_modidate) {
+		this.b_modidate = b_modidate;
+	}
+	public String getB_cate1() {
+		return b_cate1;
+	}
+	public void setB_cate1(String b_cate1) {
+		this.b_cate1 = b_cate1;
+	}
+	public String getB_cate2() {
+		return b_cate2;
+	}
+	public void setB_cate2(String b_cate2) {
+		this.b_cate2 = b_cate2;
+	}
+	public String getB_file() {
+		return b_file;
+	}
+	public void setB_file(String b_file) {
+		this.b_file = b_file;
+	}
+}
