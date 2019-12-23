6bb5ffd12a3341ba79968fe4f3336d306c306777
2Class08 <2Class08@2Class07>
2019-12-23 16:56:21 +0900

    글삭제

--- src/main/java/com/tptp/dao/ODAO.java
+++ src/main/java/com/tptp/dao/ODAO.java
@@ -12,6 +12,7 @@ public class ODAO extends AbstractDAO {
 
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> notice(Map<String, Object> map) {
+		System.out.println(map.get("notice"));
 		return selectList("o.notice", map);
 	}
 
@@ -41,6 +42,10 @@ public class ODAO extends AbstractDAO {
 
 	public void countUp(int num) {
 		update("o.countUp", num);
+	}
+
+	public int delete(Map<String, Object> map) {
+		return (int) delete("o.delete", map);
 	}
 
 }
