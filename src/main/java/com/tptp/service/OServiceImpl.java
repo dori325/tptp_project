5f766ff9c20a7346451b8d1dbe7150a08343b737
dori325 <58843330+dori325@users.noreply.github.com>
2019-12-26 15:27:38 +0900

    Add files via upload

---
+++ src/main/java/com/tptp/service/OServiceImpl.java
@@ -0,0 +1,64 @@
+package com.tptp.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.tptp.dao.ODAO;
+
+@Service("oService")
+public class OServiceImpl implements OService  {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name = "oDAO")
+	private ODAO oDAO;
+	
+	@Override
+	public List<Map<String, Object>> notice(Map<String, Object> map) throws Exception {
+		return oDAO.notice(map);
+	}
+	@Override
+	public List<Map<String, Object>> brand1(Map<String, Object> map) throws Exception {
+		return oDAO.brand1(map);
+	}
+	@Override
+	public List<Map<String, Object>> brand2(Map<String, Object> map) throws Exception {
+		return oDAO.brand2(map);
+	}
+	@Override
+	public List<Map<String, Object>> brand3(Map<String, Object> map) throws Exception {
+		return oDAO.brand3(map);
+	}
+	@Override
+	public List<Map<String, Object>> commdetail(Map<String, Object> map) throws Exception {
+		return oDAO.commdetail(map);
+	}
+	@Override
+	public Map<String, Object> detail(Map<String, Object> map) throws Exception {
+		return oDAO.detail(map);
+	}
+	@Override
+	public int writeInsert(Map<String, Object> map) throws Exception {
+		return oDAO.writeInsert(map);
+	}
+	@Override
+	public void countUp(int num) throws Exception {
+		oDAO.countUp(num);
+	}
+	@Override
+	public int delete(Map<String, Object> map) throws Exception {
+		return oDAO.delete(map);
+	}
+	@Override
+	public Map<String, Object> update(Map<String, Object> map) throws Exception {
+		return oDAO.update(map);
+	}
+	@Override
+	public int updateInsert(Map<String, Object> map) throws Exception {
+		return oDAO.updateInsert(map);
+	}
+}
